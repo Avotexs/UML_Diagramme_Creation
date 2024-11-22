f23add1e6159859c2c8d83c6e0b42c6776c54a64
TAHA <TAHA@DESKTOP-93VVBV0>
2024-11-22 18:46:31 +0100

    Form Add Class

---
+++ UML_Diagramme_Creation/AddClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UML_Diagramme_Creation
+{
+    public partial class AddClass : Form
+    {
+        public AddClass()
+        {
+            InitializeComponent();
+        }
+
+   
+    }
+}
